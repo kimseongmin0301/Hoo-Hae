15d43950bc81eb425c3c786e69676a60d56d1840
kimseongmin0301 <hippo159753@gmail.com>
2024-09-28 02:34:42 +0900

    :sparkles: git action setting

--- back/src/main/java/com/poten/hoohae/client/controller/BoardController.java
+++ back/src/main/java/com/poten/hoohae/client/controller/BoardController.java
@@ -1,8 +1,12 @@
 package com.poten.hoohae.client.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class BoardController {
 
+    @GetMapping("/index")
     public String index() {
         return "index";
     }

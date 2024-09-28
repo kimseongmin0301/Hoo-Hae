422eb8539f4094a0c9f7c7933804c2b2bb150503
kimseongmin0301 <hippo159753@gmail.com>
2024-09-28 18:34:43 +0900

    :sparkles: git action test

--- back/src/main/java/com/poten/hoohae/auth/domain/User.java
+++ back/src/main/java/com/poten/hoohae/auth/domain/User.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Table(name = "USER")
 @Entity
 @Getter
@@ -22,4 +24,21 @@ public class User {
 
     @Column(name = "NICK_NAME")
     private String nickName;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "UDPATED_AT")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected  void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }

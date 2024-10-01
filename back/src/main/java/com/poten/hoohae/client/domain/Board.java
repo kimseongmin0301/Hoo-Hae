99461492df163b274ec82e287941f5dbe936e7e9
kimseongmin0301 <hippo159753@gmail.com>
2024-10-01 19:51:18 +0900

    :sparkles: kakao login 구현, 토큰검증, 게시글 조회 테스트

--- back/src/main/java/com/poten/hoohae/client/domain/Board.java
+++ back/src/main/java/com/poten/hoohae/client/domain/Board.java
@@ -1,4 +1,68 @@
 package com.poten.hoohae.client.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "BOARD")
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USER_ID")
+    private String userId;
+
+    @Column(name = "NICKNAME")
+    private String nickanme;
+
+    @Column(name = "SUBJECT")
+    private String subject;
+
+    @Column(name = "BODY")
+    private String body;
+
+    @Column(name = "VOTE")
+    private Long vote;
+
+    @Column(name = "ADOPTION_ID")
+    private Long adoptionId;
+
+    @Column(name = "THUMBNAIL")
+    private Long thumbnail;
+
+    @Column(name = "AGE")
+    private Long age;
+
+    @Column(name = "CATEGORY")
+    private Long category;
+
+    @Column(name = "TYPE")
+    private String type;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
--- back/src/main/java/com/poten/hoohae/client/repository/BoardRepository.java
+++ back/src/main/java/com/poten/hoohae/client/repository/BoardRepository.java
@@ -1,4 +1,16 @@
 package com.poten.hoohae.client.repository;
 
-public class BoardRepository {
+import com.poten.hoohae.client.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAll(Pageable pageable);
+
+    Page<Board> findAllByAge(Pageable pageable, Long age);
+
+    long countBoardsByAge(long age);
 }

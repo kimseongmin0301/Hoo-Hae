d486f913fdc5cf9f46722c31db60a7839841cb46
kimseongmin0301 <hippo159753@gmail.com>
2024-10-03 00:44:45 +0900

    :sparkles: front init

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <section id="container">
+		<router-view />
+		<LayoutFooter msg="홍길동" v-if="getMenuInfo.isShowFooter" />
+ </section>
+</template>
+
+<script>
+import LayoutFooter from './components/LayoutFooter.vue'
+import { mapGetters} from "vuex";
+
+export default {
+  name: 'App',
+  components: {
+	LayoutFooter,
+  },
+  computed: {
+    ...mapGetters([
+      "getMenuInfo"
+    ]),
+  },
+}
+</script>
+
+<style>
+@import './assets/css/app.css';
+</style>

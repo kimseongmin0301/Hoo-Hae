27ff5558aa8b15cb201ac634943e8ce11cc644a9
kimseongmin0301 <hippo159753@gmail.com>
2024-10-03 12:15:25 +0900

    :lock: cors

--- back/src/main/java/com/poten/hoohae/auth/config/CorsConfig.java
+++ back/src/main/java/com/poten/hoohae/auth/config/CorsConfig.java
@@ -1,25 +1,25 @@
-package com.poten.hoohae.auth.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://www.hoohae.com");
-        config.addAllowedOrigin("https://hoohae.com");
-        config.addAllowedHeader("*");
-        config.addExposedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-}
+//package com.poten.hoohae.auth.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("https://www.hoohae.com");
+//        config.addAllowedOrigin("https://hoohae.com");
+//        config.addAllowedHeader("*");
+//        config.addExposedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//
+//}

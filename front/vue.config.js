6771e0ee3c565c836cc78b5b93ef5546144cb7c3
kimseongmin0301 <hippo159753@gmail.com>
2024-10-03 17:42:24 +0900

    :bug: vue.config.js 포트 설정 추가

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port: 3000,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://211.188.52.53:39880',

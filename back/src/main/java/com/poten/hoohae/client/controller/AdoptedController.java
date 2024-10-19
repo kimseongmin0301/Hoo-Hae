ee1cb597479905d0ba46e6305a75b571c53f08f5
kimseongmin0301 <hippo159753@gmail.com>
2024-10-19 16:59:57 +0900

    :sparkles: 알림, 채택 구현

--- back/src/main/java/com/poten/hoohae/client/controller/AdoptedController.java
+++ back/src/main/java/com/poten/hoohae/client/controller/AdoptedController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +15,7 @@ public class AdoptedController {
     private final AdoptedService adoptedService;
 
     @PostMapping("/{commentId}")
-    public ResponseEntity<?> adopted(@RequestParam("commentId") Long id, Authentication authentication){
+    public ResponseEntity<?> adopted(@PathVariable("commentId") Long id, Authentication authentication){
 
         return ResponseEntity.ok(adoptedService.adopted(id, authentication.getName()));
     }

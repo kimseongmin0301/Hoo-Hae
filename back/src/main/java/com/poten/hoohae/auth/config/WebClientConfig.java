3421075b5bcc2ec6eb93b05b3e422fcb3c6f479d
kimseongmin0301 <hippo159753@gmail.com>
2024-10-19 14:22:19 +0900

    :sparkles: 알림 list, delete 개발

---
+++ back/src/main/java/com/poten/hoohae/auth/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.poten.hoohae.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}

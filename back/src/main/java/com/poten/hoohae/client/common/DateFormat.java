4a3bd80fe31909f6815eb74d8bfc7a15cf9101b0
kimseongmin0301 <hippo159753@gmail.com>
2024-10-31 19:04:59 +0900

    :memo: date 형식 수정 한글

--- back/src/main/java/com/poten/hoohae/client/common/DateFormat.java
+++ back/src/main/java/com/poten/hoohae/client/common/DateFormat.java
@@ -2,10 +2,11 @@ package com.poten.hoohae.client.common;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateFormat {
     public static String yyyyMMdd(LocalDateTime date) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd(E) HH:mm");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd(E) HH:mm", Locale.KOREAN);
         return date.format(dateFormatter);
     }
 }

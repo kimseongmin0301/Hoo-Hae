995560f73df94fd1bd940909d68fc4a696d1a455
kimseongmin0301 <hippo159753@gmail.com>
2024-11-06 18:57:05 +0900

    :bug: alarm cnt 수정

--- back/src/main/java/com/poten/hoohae/client/repository/AlarmRepository.java
+++ back/src/main/java/com/poten/hoohae/client/repository/AlarmRepository.java
@@ -13,7 +13,9 @@ import java.util.List;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-    long countByUserId(String userId);
+
+    @Query("select count(a) from Alarm a where a.userId = :userId and a.status = 1")
+    long countByUserId(@Param("userId") String userId);
 
     Page<Alarm> findAllByUserIdOrderByIdDesc(Pageable page, String userId);
 
